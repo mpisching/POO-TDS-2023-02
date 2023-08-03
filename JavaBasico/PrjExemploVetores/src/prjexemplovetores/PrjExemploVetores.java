19cc4c8b05738218b9e0e7d0a405630e72210ff7
Marcos André Pisching <mpisching@gmail.com>
2023-08-03 16:29:31 -0300

    Atualização dos arquivos da aula do dia 03/08/2023

--- JavaBasico/PrjExemploVetores/src/prjexemplovetores/PrjExemploVetores.java
+++ JavaBasico/PrjExemploVetores/src/prjexemplovetores/PrjExemploVetores.java
@@ -16,51 +16,66 @@ public class PrjExemploVetores {
     
     static Scanner entrada = new Scanner(System.in);
     static final int TAM = 5;
-    static int[] notas = new int[TAM];
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //declaração de vetor
+        // tipo_de_dado[] nomeVariavelVetor = new tipo_de_dado[TAM];
+        //int[] notas = {8, 9, 10, 5, 3};//new int[5]
+        int[] notas = new int[TAM];
+        
+        //entrada de dados
+        //o que não se deve fazer
+//        System.out.println("Informe a primeira nota");
+//        notas[0] = entrada.nextInt();
+//        System.out.println("Informe a segunda nota");
+//        notas[1] = entrada.nextInt();
+//        System.out.println("Informe a terceira nota");
+//        notas[2] = entrada.nextInt();
+        //solução mais adequada para a entrada de dados em vetor
         
         System.out.println("Você deseja iniciar o vetor automaticamnte (1) ou digitar as notas (2)");
         int opcao = entrada.nextInt();
         switch(opcao) {
             case 1: 
-                inicializarVetor();
+                notas = inicializarVetor();
                 break;
             case 2: 
-                solicitarEntrada();
+                notas = solicitarEntrada();
                 break;
             default: System.out.println("Opção invalida"); System.exit(0);
         }
        
         //for aprimorado do java
-        imprimir();
+        imprimir(notas);
         
         
     }
 
-    private static void imprimir() {
+    private static void imprimir(int[] notas) {
         int cont = 0;
         for (int nota: notas) {
             System.out.println("Nota " + (++cont) + ": " + nota);
         }
     }
 
-    private static void solicitarEntrada() {
+    private static int[] solicitarEntrada() {
         int[] notas = new int[TAM];
         for (int i = 0; i < notas.length; i++) {
             System.out.print("Informe a nota " + (i + 1) + ": ");
             notas[i] = entrada.nextInt();
         }
+        return notas;
     }
 
-    private static void inicializarVetor() {
+    private static int[] inicializarVetor() {
+        int[] notas = new int[TAM];
         Random r = new Random();
         for (int i = 0; i < notas.length; i++) {
             notas[i] = r.nextInt(11);
         }
-        
+        return notas;
     }
     
 }
--- JavaBasico/PrjExemploVetores_VetorGlobal/src/prjexemplovetores/PrjExemploVetores.java
+++ JavaBasico/PrjExemploVetores_VetorGlobal/src/prjexemplovetores/PrjExemploVetores.java
@@ -16,66 +16,46 @@ public class PrjExemploVetores {
     
     static Scanner entrada = new Scanner(System.in);
     static final int TAM = 5;
-    /**
-     * @param args the command line arguments
-     */
+    static int[] notas = new int[TAM];
+    
     public static void main(String[] args) {
-        //declaração de vetor
-        // tipo_de_dado[] nomeVariavelVetor = new tipo_de_dado[TAM];
-        //int[] notas = {8, 9, 10, 5, 3};//new int[5]
-        int[] notas = new int[TAM];
-        
-        //entrada de dados
-        //o que não se deve fazer
-//        System.out.println("Informe a primeira nota");
-//        notas[0] = entrada.nextInt();
-//        System.out.println("Informe a segunda nota");
-//        notas[1] = entrada.nextInt();
-//        System.out.println("Informe a terceira nota");
-//        notas[2] = entrada.nextInt();
-        //solução mais adequada para a entrada de dados em vetor
-        
         System.out.println("Você deseja iniciar o vetor automaticamnte (1) ou digitar as notas (2)");
         int opcao = entrada.nextInt();
         switch(opcao) {
             case 1: 
-                notas = inicializarVetor();
+                inicializarVetor();
                 break;
             case 2: 
-                notas = solicitarEntrada();
+                solicitarEntrada();
                 break;
             default: System.out.println("Opção invalida"); System.exit(0);
         }
        
         //for aprimorado do java
-        imprimir(notas);
+        imprimir();
         
         
     }
 
-    private static void imprimir(int[] notas) {
+    private static void imprimir() {
         int cont = 0;
         for (int nota: notas) {
             System.out.println("Nota " + (++cont) + ": " + nota);
         }
     }
 
-    private static int[] solicitarEntrada() {
-        int[] notas = new int[TAM];
+    private static void solicitarEntrada() {
         for (int i = 0; i < notas.length; i++) {
             System.out.print("Informe a nota " + (i + 1) + ": ");
             notas[i] = entrada.nextInt();
         }
-        return notas;
     }
 
-    private static int[] inicializarVetor() {
-        int[] notas = new int[TAM];
+    private static void inicializarVetor() {
         Random r = new Random();
         for (int i = 0; i < notas.length; i++) {
             notas[i] = r.nextInt(11);
         }
-        return notas;
     }
     
 }
