ec2429ab1940ece2b3fbfa62d928fcc1c98a93a1
Marcos André Pisching <mpisching@gmail.com>
2023-08-10 18:22:10 -0300

    Aula do dia 10/08/2023
    JavaFX - Primeiro projeto JavaFX

---
+++ JavaFX/PrjJavaFXExemplo1/src/mainapp/JavaFXMain.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package mainapp;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author mpisc
+ */
+public class JavaFXMain extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("MainFXML.fxml"));
+        } catch (IOException ex) {
+            System.out.println("Não foi possível carregar o formulário");
+        }
+
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Primeiro Projeto JavaFX!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
